333d2f8baf5c4b77b6b567cbd403c7cea9098bd0
Quirin Heiß <qheiss94@gmail.com>
2019-06-12 14:35:40 +0200

    bla

--- testproject/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testproject.settings')
-
-app = Celery('testproject.polls')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
