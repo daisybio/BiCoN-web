8982d70e4b41edcf8ec6f7835242de362b228ebe
Quirin Heiß <qheiss94@gmail.com>
2019-06-12 14:30:22 +0200

    bla

---
+++ clust_app/celery.py
@@ -0,0 +1,10 @@
+import os
+from celery import Celery
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clust_app.settings')
+
+#app = Celery('testproject.polls')
+app = Celery('clust_app.clustering')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
