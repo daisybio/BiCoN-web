1a5075b87e0ea4cc7f8c1507888a558dce1ff614
Kevin Yuan <kevihiiin@users.noreply.github.com>
2019-10-28 23:42:07 +0100

    Complete js input form validation

--- static/clustering/js/forms.js
+++ static/clustering/js/forms.js
@@ -63,7 +63,24 @@ function validateForm() {
 
         }
 
-        // Check the expression tab
+        // If predefined option is used, disable metadata selection options
+        if (!document.getElementById('expression-data_3').checked) {
+            // Mute text
+            $('#metadata_options').addClass('text-muted')
+            // Disable radios and select yes
+            $('input[name=use_metadata]').attr("disabled", true);
+            $('input[name=use_metadata]').val(['yes'])
+            $('#use_metadata_info').addClass('d-block')
+        } else {
+            // Unmute text
+            $('#metadata_options').removeClass('text-muted')
+            // Enable radios and select no
+            $('input[name=use_metadata]').attr("disabled", false);
+            $('input[name=use_metadata]').prop('checked', false);
+            $('#use_metadata_info').removeClass('d-block')
+        }
+
+        // Check the ppi tab
     } else if (y.classList.contains("tab-ppi")) {
         // Check if no option has been selected, set to false
         if ($('input[name=ppi-network]:checked').length === 0) {
@@ -80,34 +97,105 @@ function validateForm() {
             $('#ppi_file_err').removeClass('d-block')
 
         }
+
+        // Metadata tab
+    } else if (y.classList.contains("tab-metadata")) {
+        // If metadata is not selected or disabled do nothing
+        if ($('input[name=use_metadata]:checked', '#regForm').val() === 'yes'
+            && !$('input[name=use_metadata]').is('[disabled=disabled]')) {
+            // Check if column name is given
+            if (!$('#survival-col').val().trim()) {
+                $('#survival-col_err').addClass('d-block')
+                valid = false
+            } else {
+                $('#survival-col_err').removeClass('d-block')
+            }
+
+            // Check if upload file is given
+            if (document.getElementById("survival-metadata").value === "") {
+                $('#survival-metadata_err').addClass('d-block')
+                valid = false
+            } else {
+                $('#survival-metadata_err').removeClass('d-block')
+            }
+        }
+
+
+        // Check algorithm parameter tab
     } else if (y.classList.contains("tab-parameters")) {
-      $('#L_g_min').val()
+        const L_g_min = $('#L_g_min').val();
+        const L_g_max = $('#L_g_max').val();
 
-    }
-        // // A loop that checks every input field in the current tab:
-        // for (i = 0; i < y.length; i++) {
-        //     // If a field is empty...
-        //     if (y[i].value == "") {
-        //         // add an "invalid" class to the field:
-        //         y[i].className += " invalid";
-        //         // and set the current valid status to false:
-        //         valid = false;
-        //     }
-        // }
-        // If the valid status is true, mark the step as finished and valid:
+        // Check if L_g_min is a number
+        if (!isNumber(L_g_min)) {
+            valid = false;
+            $('#L_g_min_err').addClass('d-block')
+        } else {
+            $('#L_g_min_err').removeClass('d-block')
+        }
+
+        // Check if L_g_max is a number
+        if (!isNumber(L_g_max)) {
+            valid = false;
+            $('#L_g_max_err').addClass('d-block')
+        } else {
+            $('#L_g_max_err').removeClass('d-block')
+        }
+
+        // Check if min is smaller or equal to max
         if (valid) {
-            document.getElementsByClassName("step")[currentTab].className += " finish";
+            if (parseInt(L_g_min) > parseInt(L_g_max)) {
+                $('#cluster_param_error').addClass('d-block')
+                valid = false;
+            } else {
+                $('#cluster_param_error').removeClass('d-block')
+            }
         }
-        return valid; // return the valid status
-        // return true; // return the valid status
-    }
 
-    function fixStepIndicator(n) {
-        // This function removes the "active" class of all steps...
-        var i, x = document.getElementsByClassName("step");
-        for (i = 0; i < x.length; i++) {
-            x[i].className = x[i].className.replace(" active", "");
+        // If advanced options are selected
+        if ($('input[name=clustering_advanced]:checked', '#regForm').val() === 'yes') {
+            const gene_set_size = $('#gene_set_size').val();
+            const nbr_iter = $('#nbr_iter').val();
+
+            // Check if gene_set_size is a number
+            if (!isNumber(gene_set_size)) {
+                valid = false;
+                $('#gene_set_size_err').addClass('d-block')
+            } else {
+                $('#gene_set_size_err').removeClass('d-block')
+            }
+
+            // Check if nbr_iter is a number
+            if (!isNumber(nbr_iter)) {
+                valid = false;
+                $('#nbr_iter_err').addClass('d-block')
+            } else {
+                $('#nbr_iter_err').removeClass('d-block')
+            }
+
+
         }
-        //... and adds the "active" class to the current step:
-        x[n].className += " active";
+
+
     }
+    if (valid) {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+    }
+    return valid; // return the valid status
+    // return true; // return the valid status
+}
+
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && !isNaN(n - 0)
+}
+
+
+function fixStepIndicator(n) {
+    // This function removes the "active" class of all steps...
+    var i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
+    }
+    //... and adds the "active" class to the current step:
+    x[n].className += " active";
+}
