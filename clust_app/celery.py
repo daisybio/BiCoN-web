437bea1f43592afb88ed7a92187b3d725c62e0af
Kevin Yuan <kevihiiin@users.noreply.github.com>
2019-11-07 20:22:22 +0100

    Preparing for merge with new_structure branch

--- clust_app/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clust_app.settings')
-
-app = Celery('clust_app.clustering')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
